cda37f944ff94d161ef2ac816d1edc9a26677aec
Dariusz Bosko <dariuszbosko95@gmail.com>
2026-08-11 14:48:47 +0200

    add team permissions

--- src/main/java/com/poorbet/authstarter/security/PoorbetPermissions.java
+++ src/main/java/com/poorbet/authstarter/security/PoorbetPermissions.java
@@ -3,6 +3,9 @@ package com.poorbet.authstarter.security;
 public final class PoorbetPermissions {
 
     public static final String COUPON_CREATE = "coupon:create";
+    public static final String TEAM_CREATE = "team:create";
+    public static final String TEAM_READ = "team:read";
+    public static final String TEAM_UPDATE = "team:update";
     public static final String MATCH_ODDS_READ = "match:odds:read";
     public static final String INTERNAL_READ = "internal:read";
     public static final String INTERNAL_WRITE = "internal:write";
